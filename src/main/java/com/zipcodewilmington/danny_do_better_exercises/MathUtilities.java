ea3f93c315853347a98a49897ee0eb9416d8105b
eugeneshine1 <eugeneshine1@gmail.com>
2022-06-30 20:02:18 -0400

    passed

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -11,7 +11,7 @@ public class MathUtilities {
      * @return sum of `baseValue` and `difference`
      */
     public Integer add(int baseValue, int difference) {
-        return null;
+        return baseValue+difference;
     }
 
     /**
@@ -19,8 +19,7 @@ public class MathUtilities {
      * @param difference value to add to starting value
      * @return sum of `baseValue` and `difference`
      */
-    public Long add(long baseValue, long difference) {
-        return null;
+    public Long add(long baseValue, long difference) {return baseValue+difference;
     }
 
     /**
@@ -29,7 +28,7 @@ public class MathUtilities {
      * @return sum of `baseValue` and `difference`
      */
     public Short add(short baseValue, short difference) {
-        return null;
+        return (short)(baseValue +difference);
     }
 
     /**
@@ -38,7 +37,7 @@ public class MathUtilities {
      * @return sum of `baseValue` and `difference`
      */
     public Byte add(byte baseValue, byte difference) {
-        return null;
+        return (byte)(baseValue+difference);
     }
 
     /**
@@ -46,8 +45,7 @@ public class MathUtilities {
      * @param difference value to add to starting value
      * @return sum of `baseValue` and `difference`
      */
-    public Float add(float baseValue, float difference) {
-        return null;
+    public Float add(float baseValue, float difference) {return baseValue+difference;
     }
 
     /**
@@ -55,8 +53,7 @@ public class MathUtilities {
      * @param difference value to add to starting value
      * @return sum of `baseValue` and `difference`
      */
-    public Double add(double baseValue, double difference) {
-        return null;
+    public Double add(double baseValue, double difference) {return baseValue+difference;
     }
 
     /**
@@ -65,7 +62,7 @@ public class MathUtilities {
      * @return difference between `baseValue` and `difference`
      */
     public Integer subtract(int baseValue, int difference) {
-        return null;
+        return baseValue-difference;
     }
 
     /**
@@ -73,8 +70,7 @@ public class MathUtilities {
      * @param difference value to subtract from starting value
      * @return difference between `baseValue` and `difference`
      */
-    public Long subtract(long baseValue, long difference) {
-        return null;
+    public Long subtract(long baseValue, long difference) {return baseValue-difference;
     }
 
     /**
@@ -83,7 +79,7 @@ public class MathUtilities {
      * @return difference between `baseValue` and `difference`
      */
     public Short subtract(short baseValue, short difference) {
-        return null;
+        return (short) (baseValue-difference);
     }
 
     /**
@@ -91,8 +87,7 @@ public class MathUtilities {
      * @param difference value to subtract from starting value
      * @return difference between `baseValue` and `difference`
      */
-    public Byte subtract(byte baseValue, byte difference) {
-        return null;
+    public Byte subtract(byte baseValue, byte difference) {return (byte)(baseValue-difference);
     }
 
     /**
@@ -100,8 +95,7 @@ public class MathUtilities {
      * @param difference value to subtract from starting value
      * @return difference between `baseValue` and `difference`
      */
-    public Float subtract(float baseValue, float difference) {
-        return null;
+    public Float subtract(float baseValue, float difference) {return baseValue - difference;
     }
 
     /**
@@ -110,7 +104,7 @@ public class MathUtilities {
      * @return difference between `baseValue` and `difference`
      */
     public Double subtract(double baseValue, double difference) {
-        return null;
+        return baseValue - difference;
     }
 
 
@@ -119,8 +113,7 @@ public class MathUtilities {
      * @param divisor value to divide by
      * @return division of `dividend` by `divisor
      */
-    public Integer divide(int dividend, int divisor) {
-        return null;
+    public Integer divide(int dividend, int divisor) {return dividend/divisor;
     }
 
     /**
@@ -128,8 +121,7 @@ public class MathUtilities {
      * @param divisor value to divide by
      * @return division of `dividend` by `divisor
      */
-    public Long divide(long dividend, long divisor) {
-        return null;
+    public Long divide(long dividend, long divisor) {return dividend/divisor;
     }
 
     /**
@@ -137,8 +129,7 @@ public class MathUtilities {
      * @param divisor value to divide by
      * @return division of `dividend` by `divisor
      */
-    public Short divide(short dividend, short divisor) {
-        return null;
+    public Short divide(short dividend, short divisor) {return (short)(dividend/divisor);
     }
 
     /**
@@ -146,8 +137,7 @@ public class MathUtilities {
      * @param divisor value to divide by
      * @return division of `dividend` by `divisor
      */
-    public Byte divide(byte dividend, byte divisor) {
-        return null;
+    public Byte divide(byte dividend, byte divisor) {return (byte)(dividend/divisor);
     }
 
     /**
@@ -155,8 +145,7 @@ public class MathUtilities {
      * @param divisor value to divide by
      * @return division of `dividend` by `divisor
      */
-    public Float divide(float dividend, float divisor) {
-        return null;
+    public Float divide(float dividend, float divisor) {return dividend/divisor;
     }
 
     /**
@@ -165,7 +154,7 @@ public class MathUtilities {
      * @return division of `dividend` by `divisor
      */
     public Double divide(double dividend, double divisor) {
-        return null;
+        return dividend/divisor;
     }
 
 
@@ -175,7 +164,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Integer multiply(int multiplicand, int multiplier) {
-        return null;
+        return multiplicand*multiplier;
     }
 
     /**
@@ -184,7 +173,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Long multiply(long multiplicand, long multiplier) {
-        return null;
+        return multiplicand*multiplier;
     }
 
     /**
@@ -193,7 +182,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Short multiply(short multiplicand, short multiplier) {
-        return null;
+        return (short)(multiplicand*multiplier);
     }
     /**
      * @param multiplicand value to be multiplied
@@ -201,7 +190,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Byte multiply(byte multiplicand, byte multiplier) {
-        return null;
+        return (byte)(multiplicand*multiplier);
     }
 
     /**
@@ -210,7 +199,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Float multiply(float multiplicand, float multiplier) {
-        return null;
+        return multiplicand*multiplier;
     }
 
     /**
@@ -219,6 +208,6 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Double multiply(double multiplicand, double multiplier) {
-        return null;
+        return multiplicand*multiplier;
     }
 }
--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -8,7 +8,7 @@ public class StringUtilities {
      * @return `Hello World` as a string
      */
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
 
     /**
@@ -17,7 +17,7 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+        return firstSegment.concat(secondSegment);
     }
 
     /**
@@ -26,7 +26,9 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+       StringBuilder s = new StringBuilder();
+       s.append(firstSegment);
+        return s.toString().concat(secondSegment);
     }
 
     /**
@@ -34,7 +36,12 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+        input.split("");
+        StringBuilder s = new StringBuilder();
+        s.append(input.charAt(0));
+        s.append(input.charAt(1));
+        s.append(input.charAt(2));
+        return s.toString();
     }
 
     /**
@@ -42,7 +49,12 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+        input.split("");
+        StringBuilder s = new StringBuilder();
+        s.append(input.charAt(input.length()-3));
+        s.append(input.charAt(input.length()-2));
+        s.append(input.charAt(input.length()-1));
+        return s.toString();
     }
 
     /**
@@ -50,8 +62,12 @@ public class StringUtilities {
      * @param comparableValue the value to be compared against
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
-    public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+    public static Boolean compareTwoStrings(String inputValue, String comparableValue) {
+        if (inputValue==comparableValue) {
+            return true;
+        } else{
+            return false;
+        }
     }
 
     /**
@@ -59,7 +75,8 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        int n = (inputValue.length() -1)/2;
+        return inputValue.charAt(n);
     }
 
     /**
@@ -67,7 +84,9 @@ public class StringUtilities {
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+        String [] s = spaceDelimitedString.split(" ");
+        String sd = s[0];
+        return sd;
     }
 
     /**
@@ -75,7 +94,9 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String [] s = spaceDelimitedString.split(" ");
+        String sd = s[1];
+        return sd;
     }
 
     /**
@@ -83,6 +104,9 @@ public class StringUtilities {
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+        StringBuilder s = new StringBuilder();
+        s.append(stringToReverse);
+        s.reverse();
+        return s.toString();
     }
 }
